24892bda9bbfad6924bbbcf6b73b335cff1197df
Ohffs <ohffsnotnow@gmail.com>
2024-07-18 20:35:25 +0100

    Add new gpt-4o-mini model

--- gepetto/gpt.py
+++ gepetto/gpt.py
@@ -8,6 +8,7 @@ class Model(Enum):
     GPT4_32k = ('gpt-4-32k', 0.03, 0.06)
     GPT_4_1106_PREVIEW = ('gpt-4-1106-preview', 0.01, 0.03)
     GPT_4_TURBO = ('gpt-4-turbo', 0.01, 0.03)
+    GPT_4_OMNI_MINI = ('gpt-4o-mini', 0.000150, 0.000075)
     GPT_4_OMNI = ('gpt-4o', 0.005, 0.015)
     GPT4 = ('gpt-4', 0.06, 0.12)
     GPT3_5_Turbo_gpt_1106 = ('gpt-3.5-turbo-1106', 0.001, 0.002)
@@ -18,7 +19,7 @@ class GPTModel():
     name = "Gepetto"
     uses_logs = True
 
-    def get_token_price(self, token_count, direction="output", model_engine="gpt-4o"):
+    def get_token_price(self, token_count, direction="output", model_engine="gpt-4o-mini"):
         token_price_input = 0
         token_price_output = 0
         for model in Model:
@@ -30,7 +31,7 @@ class GPTModel():
             return round(token_price_input * token_count, 4)
         return round(token_price_output * token_count, 4)
 
-    async def chat(self, messages, temperature=1.8, model="gpt-4o", top_p=0.6):
+    async def chat(self, messages, temperature=1.8, model="gpt-4o-mini", top_p=0.6):
         """Chat with the model.
 
         Args:
