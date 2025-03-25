e8a2dba6d9dad07060e3c0ab2a5d37687a835725
Ohffs <ohffsnotnow@gmail.com>
2025-03-25 20:30:54 +0000

    Faff

--- gepetto/gemini.py
+++ gepetto/gemini.py
@@ -8,5 +8,5 @@ from .base_model import BaseModel
 class GeminiModel(BaseModel):
     name = "RecipeThis"
     uses_logs = False
-    default_model = "gemini-2.0-flash"
+    default_model = "gemini-2.5-pro-exp-03-25"
     provider = "gemini"
