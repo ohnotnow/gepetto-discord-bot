f4cc591b8eae08770904b258a8afe4c5afab2e9a
Ohffs <ohffsnotnow@gmail.com>
2025-04-17 17:21:43 +0100

    Faff

--- gepetto/gpt.py
+++ gepetto/gpt.py
@@ -1,7 +1,7 @@
 from .base_model import BaseModel
-
+import os
 class GPTModel(BaseModel):
     name = "Gepetto"
     uses_logs = True
-    default_model = "gpt-4.1-mini"
+    default_model = os.getenv("BOT_MODEL", "gpt-4.1-mini")
     provider = "openai"
