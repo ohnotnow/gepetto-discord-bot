e1a9ee1cc8197adc8cd0d01bee475243afb98de8
Ohffs <ohffsnotnow@gmail.com>
2025-07-07 18:58:37 +0100

    Faff

--- gepetto/websearch.py
+++ gepetto/websearch.py
@@ -13,4 +13,18 @@ async def websearch(query: str, search_context_size: str = "medium"):
             "search_context_size": "medium"  # Options: "low", "medium", "high"
         }
     )
+    response_text = response.choices[0].message.content
+    response = await acompletion(
+        model="openai/gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that summarises the web search results for the user. You MUST keep the markdown links to the sources, but give a concise summary of the text around them.  Your response will be sent to a discord server so you only have about 1800 characters in total.",
+            },
+            {
+                "role": "user",
+                "content": response_text,
+            }
+        ]
+    )
     return response.choices[0].message.content[:1800]
